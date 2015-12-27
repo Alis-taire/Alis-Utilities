281d57fedf24898a527749e36f939360b179eb46
Alis-taire <Alis-taire@users.noreply.github.com>
2015-12-26 19:55:43 -0500

    Update getService.lua

--- getService.lua
+++ getService.lua
@@ -1,5 +1,7 @@
 
 local Service = { };
+local preferedStorage = game:GetService("ReplicatedStorage")
+local Players = game:GetService("Players")
 
 function GetChildren(Object, Recursive)
   local function scan_insert(k, v, recurse)
@@ -15,12 +17,35 @@ function GetChildren(Object, Recursive)
 end
 Service.GetChildren = GetChildren
 
-function GetPlayerFromPart(Part)
+function GetPlayer(Part)
   if Part:IsA("BasePart") then
     if Part.Parent and Part.Parent:IsA("Model") then
-      return game.Players:GetPlayerFromCharacter(part.Parent);
+      return Players:GetPlayerFromCharacter(part.Parent);
     end
   end
   return nil
 end
-service.GetPlayerFromPart = GetPlayerFromPart
+Service.GetPlayer = GetPlayer
+
+function GetHumanoid(obj)
+	if obj ~= nil then
+		if obj.Parent and obj.Parent:IsA("Model") then
+			return obj.Parent:findFirstChild("Humanoid")
+		end
+	end
+end
+Service.GetHumanoid = GetHumanoid
+
+function GetRemote(remoteName, remoteType, isClient)
+  if not isClient then
+      local Remote = game:FindFirstChild(remoteName, true)
+      if Remote and Remote:IsA(remoteType) then
+        return Remote
+      end
+  elseif isClient then
+    print("Yielding until found "..remoteName..remoteType..".")
+    local Remote = game[preferedStorage]:WaitForChild(remoteName)
+    return Remote
+  end
+end
+Service.GetRemote = GetRemote
